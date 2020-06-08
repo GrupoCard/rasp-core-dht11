57797fc4021f6ec32943907e3f1f6c2931453324
Fabiano Conrado <fabianoconrado@gmail.com>
2020-06-08 19:57:45 -0300

    Enviando a estrutura basica do projeto

---
+++ source/Termometro.Services.Api/Configurations/SwaggerSetup.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace Termometro.Services.Api.Configurations
+{
+    public static class SwaggerSetup
+    {
+        public static void AddSwaggerSetup(this IServiceCollection services)
+        {
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Termômetro API",
+                    Description = "Alterar infomações de portas GPIO através de API",
+                    TermsOfService = new Uri("https://example.com/terms"),
+
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Fabiano Conrado",
+                        Email = "fabianoconrado@gmail.com",
+                        Url = new Uri("https://twitter.com/fabianoconrado"),
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "MIT License",
+                        Url = new Uri("https://github.com/GrupoCard/rasp-core-dht11/blob/master/LICENSE"),
+                    }
+                });
+
+                // Set the comments path for the Swagger JSON and UI.
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+
+            });
+        }
+
+        public static void UseSwaggerSetup(this IApplicationBuilder app)
+        {
+            if (app == null) throw new ArgumentNullException(nameof(app));
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Termômetro V1 API");
+            });
+        }
+    }
+}
---
+++ source/Termometro.Services.Api/Controllers/GPIOController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Unosquare.RaspberryIO;
+using Unosquare.RaspberryIO.Abstractions;
+
+namespace Termometro.Services.Api.Controllers
+{
+    /// <summary>
+    /// CPIO Controller
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GPIOController : ControllerBase
+    {
+        /// <summary>
+        /// Fornece acesso ao Raspberry PI GPIO e inverte o seu valor
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        /// <response code="200">Alterado o </response>
+        /// <response code="400"></response>
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public bool Get(uint id)
+        {
+            var pin = Pi.Gpio[(int)id];
+            pin.PinMode = GpioPinDriveMode.Output;
+            var isOn = pin.Read();
+
+            pin.Write(!isOn);
+
+            return !isOn;
+        }
+    }
+}
